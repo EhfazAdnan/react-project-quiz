b482ba2628b3f378d33d129790ccde42cf8281dd
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-10-04 23:02:13 -0700

    html to react (31)

---
+++ src/components/Videos.js
@@ -0,0 +1,12 @@
+import classes from "../styles/Videos.module.css";
+import Video from "./Video";
+
+export default function Videos() {
+  return (
+    <div className={classes.videos}>
+      <Video />
+      <Video />
+      <Video />
+    </div>
+  );
+}
